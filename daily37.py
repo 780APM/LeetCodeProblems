493102e063973ffd7fe65f9b94d277ba197f0386
Morgan <57188236+780APM@users.noreply.github.com>
2024-01-21 19:14:57 +0100

    House Robber python

---
+++ daily37.py
@@ -0,0 +1,26 @@
+# House robber problem
+# Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+# Example 1:
+
+# Input: nums = [1,2,3,1]
+# Output: 4
+# Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+
+from typing import List
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0] * (n + 1)
+        dp[1] = nums[0]
+        for i in range(2, n + 1):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1])
+        return dp[-1]
+    
+# Tests
+solution = Solution()
+nums = [1,2,3,1]
+print(solution.rob(nums))
+
+
